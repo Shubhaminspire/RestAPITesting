4a7f717111cd5e9a2a8a84c275d6bdbd351c3e92
Shubham Sharma <shubhamsharma@Shubhams-MacBook-Air.local>
2022-09-08 15:16:21 +0530

    Added Request Response Spec Builder to Optimze Code

--- src/test/java/bodyinfo/AddPlaceData.java
+++ src/test/java/bodyinfo/AddPlaceData.java
@@ -1,5 +1,11 @@
 package bodyinfo;
 
+import com.github.dockerjava.core.dockerfile.DockerfileStatement;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import pojo.AddPlace;
 import pojo.Location;
 
@@ -7,7 +13,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddPlaceData {
+    public static String Url = "https://rahulshettyacademy.com";
     private static List<String> types = new ArrayList<>(List.of("shoe park", "shoe"));
+
+    // Request Spec Builder that can be used anywhere where we are sending common chaining across Test
+    public static RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri(Url)
+            .addQueryParam("key", "qaclick123")
+            .setContentType(ContentType.JSON).build();
+
+    // Request Spec Builder that can be used anywhere where we are sending common chaining across Test
+    public static ResponseSpecification resSpec = new ResponseSpecBuilder().expectStatusCode(200)
+            .expectContentType(ContentType.JSON).build();
 
     public static AddPlace addPlace() {
 
@@ -24,8 +40,6 @@ public class AddPlaceData {
         place.setLanguage("Indian");
         place.setTypes(types);
         place.setLocation(loc);
-
         return place;
-
     }
 }
---
+++ src/test/java/org/practise/SerializationAddPlace.java
@@ -0,0 +1,24 @@
+package org.practise;
+
+import bodyinfo.AddPlaceData;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import pojo.AddPlace;
+
+import static io.restassured.RestAssured.given;
+
+public class SerializationAddPlace extends AddPlace {
+    public static void main(String[] args) {
+
+        RequestSpecification request = given().log().all().spec(AddPlaceData.reqSpec)
+                .body(AddPlaceData.addPlace());
+
+        Response response = request.when().post("/maps/api/place/add/json").then().spec(AddPlaceData.resSpec)
+                .extract().response();
+        String extractResponse = response.asString();
+
+        System.out.println(extractResponse);
+
+    }
+
+}
